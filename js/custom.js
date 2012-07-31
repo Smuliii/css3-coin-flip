b71204163d22b56df63056e5eee23af248ab106c
Samuli Saarinen <smulitus@gmail.com>
2012-07-31 20:18:47 +0300

    First commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// If JavaScript is enabled, remove class 'no-js' and give class 'js'
+jQuery('html').removeClass('no-js').addClass('js');
+
+// Run after DOM has been fully loaded
+jQuery(document).ready(function( $ ) {
+
+	/* ---------------------------------------------------------------------- */
+	/*	Custom Functions
+	/* ---------------------------------------------------------------------- */
+
+	(function() {
+
+		
+
+	})();
+
+	/* end Custom Functions */
+
+});
